2e4fff6a387029cd7d9519331403893c18da7c62
SaadMansuri3 <41145915+SaadMansuri3@users.noreply.github.com>
2021-02-21 01:06:55 +0530

    First Commit
    -Movement
    -Shooting
    -Enemy Ai
    -Reload
    -Gun switching

---
+++ Fps Shooter/Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public Vector3 walkPoint;
+    public float walkPointRange;
+    bool walkPointSet;
+
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    public float sightRange, attackRange;
+    public bool playerInSight, playerInAttackRange;
+
+
+    public GameObject projectile;
+
+    public float health;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        playerInSight = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSight && !playerInAttackRange)
+            Patrolling();
+        if (playerInSight && !playerInAttackRange)
+            Chase();
+        if (playerInAttackRange && playerInSight)
+            Attack();
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet)
+            SearchWalkingPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToReach = transform.position - walkPoint;
+
+        if (distanceToReach.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    private void SearchWalkingPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(randomX, transform.position.y, randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void Chase()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void Attack()
+    {
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            //Attack Code
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+
+
+            //
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+            Invoke(nameof(DestroyEnemy), .5f);
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Fps Shooter/Assets/Scripts/GunScript.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public ParticleSystem smokeEffect;
+    public GameObject bulletImpact;
+    public float Damage = 10f;
+    public float Range = 100f;
+    public float impactForce = 50f;
+    public float fireRate = 15f;
+
+    public int maxAmmo = 10;
+    public float reloadTime = 1f;
+
+    private int currentAmmo;
+    private float nextTimeForFire = 0f;
+    private bool isReloading = false;
+
+    public Animator anim;
+
+    private void OnEnable()
+    {
+        isReloading = false;
+        anim.SetBool("Reloading", false);
+    }
+
+    private void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+    void Update()
+    {
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeForFire)
+        {
+            nextTimeForFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+
+        Debug.Log(currentAmmo);
+    }
+  
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        smokeEffect.Play();
+
+        currentAmmo--;
+         
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, Range))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            
+            if(target != null)
+            {
+                target.TakeDamage(Damage);
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(hit.normal*impactForce);
+            }
+
+            GameObject impactGO = Instantiate(bulletImpact, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        anim.SetBool("Reloading",true);
+        yield return new WaitForSeconds(reloadTime-.25f);
+        anim.SetBool("Reloading", false);
+        yield return new WaitForSeconds(reloadTime);
+        isReloading = false;
+        currentAmmo = maxAmmo;
+    }
+}
